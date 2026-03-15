f8c5c0fa2830873a0e5607c7f582724a9950c99f
Shoyb Hasan <shoybhasan789@gmail.com>
2026-03-15 16:19:25 +0600

    first commit

---
+++ Chapter_03-Code_Testing_Method/average.py
@@ -0,0 +1,15 @@
+def average(L):
+    if not L:
+        return None
+    return sum(L) / len(L)
+
+if __name__ == "__main__":
+    L = [1,2,3,4,5]
+    expected_result = 3.0
+    result = average(L)
+    
+    if expected_result == result:
+        print("Test passed")
+    else:
+        print("Test failed!", "recieved", result, "expected", expected_result)
+        
---
+++ Chapter_03-Code_Testing_Method/average_using_assert.py
@@ -0,0 +1,11 @@
+def average(L):
+    if not L:
+        return None
+    return sum(L) / len(L)
+
+if __name__ == "__main__":
+    L = [1,2,3,4,5]
+    expected_result = 3.0
+    result = average(L)
+    
+    assert expected_result == average(L), "average() produced incorrect result"
